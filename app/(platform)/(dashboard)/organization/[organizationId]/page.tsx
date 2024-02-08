0efccae51a59ca8c9bdb623706445e44fe335907
minhduc17801 <minhduc17801@gmail.com>
2024-02-08 09:14:47 +0700

    Form

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,17 @@
-const OrganizationPage = () => {
-  return <div>Organ</div>;
+import { Separator } from '@/components/ui/separator';
+import Info from './_components/info';
+import BoardList from './_components/board-list';
+
+const OrganizationPage = async () => {
+  return (
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList />
+      </div>
+    </div>
+  );
 };
 
 export default OrganizationPage;
