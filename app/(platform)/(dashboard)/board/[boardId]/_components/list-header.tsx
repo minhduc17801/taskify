75d85bae7aa8fe1d3be2e2c9c8cd13a8335f20d0
minhduc17801 <minhduc17801@gmail.com>
2024-02-24 23:17:28 +0700

    List header + list options

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -0,0 +1,100 @@
+'use client';
+
+import { ElementRef, useRef, useState } from 'react';
+import { useEventListener, useOnClickOutside } from 'usehooks-ts';
+import { toast } from 'sonner';
+
+import { List } from '@prisma/client';
+import { FormInput } from '@/components/form/form-input';
+import { useAction } from '@/hooks/use-action';
+import { updateList } from '@/actions/update-list';
+
+import ListOptions from './list-options';
+
+interface ListHeaderProps {
+  data: List;
+}
+
+const ListHeader = ({ data }: ListHeaderProps) => {
+  const [title, setTitle] = useState(data.title);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const formRef = useRef<ElementRef<'form'>>(null);
+  const inputRef = useRef<ElementRef<'input'>>(null);
+
+  const { execute, fieldErrors } = useAction(updateList, {
+    onSuccess: (data) => {
+      toast.success(`List "${data.title}" updated!`);
+      setTitle(data.title);
+      disableEditing();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const handleSubmit = (formData: FormData) => {
+    const title = formData.get('title') as string;
+    const id = formData.get('id') as string;
+    const boardId = formData.get('boardId') as string;
+
+    if (title === data.title) {
+      return disableEditing();
+    }
+    execute({ title, boardId, id });
+  };
+
+  const onBlur = () => {
+    formRef.current?.requestSubmit();
+  };
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+      inputRef.current?.select();
+    });
+  };
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const onKeyDown = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+      formRef.current?.requestSubmit();
+    }
+  };
+
+  useEventListener('keydown', onKeyDown);
+
+  return (
+    <div className="pt-2 px-2 text-sm font-semibold flex justify-between items-start gap-x-2">
+      {isEditing ? (
+        <form action={handleSubmit} ref={formRef} className="flex-1 px-[2px]">
+          <input hidden id="id" name="id" value={data.id} />
+          <input hidden id="boardId" name="boardId" value={data.boardId} />
+          <FormInput
+            id="title"
+            ref={inputRef}
+            onBlur={onBlur}
+            placeholder="Enter list title..."
+            defaultValue={title}
+            className="text-sm px-[7px] py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition truncate bg-transparent focus:bg-white"
+          />
+          <button type="submit" hidden></button>
+        </form>
+      ) : (
+        <div
+          onClick={enableEditing}
+          className="w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent"
+        >
+          {title}
+        </div>
+      )}
+      <ListOptions onAddCard={() => {}} data={data} />
+    </div>
+  );
+};
+
+export default ListHeader;
